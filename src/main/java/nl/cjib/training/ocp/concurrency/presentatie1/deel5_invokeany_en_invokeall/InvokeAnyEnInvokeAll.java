8a88f8e5c769e68d48eb953022ae34008605a567
CC007 <Coolcat_the_best2@hotmail.com>
2021-04-06 01:32:42 +0200

    InvokeAnyEnInvokeAll

--- src/main/java/nl/cjib/training/ocp/concurrency/presentatie1/deel5_invokeany_en_invokeall/InvokeAnyEnInvokeAll.java
+++ src/main/java/nl/cjib/training/ocp/concurrency/presentatie1/deel5_invokeany_en_invokeall/InvokeAnyEnInvokeAll.java
@@ -1,16 +1,18 @@
-package nl.cjib.training.ocp.concurrency.presentatie1.deel5_invokeall_en_any;
+package nl.cjib.training.ocp.concurrency.presentatie1.deel5_invokeany_en_invokeall;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Je kan ook meerdere {@link Callable} tasks tegelijk submitten.
  * Dit kan op meerdere manieren, afhankelijk van wat je bedoeling is.
  */
-public class ExecutorServices
+public class InvokeAnyEnInvokeAll
 {
 
 	/**
@@ -25,9 +27,17 @@ public class ExecutorServices
 
 		// Hier zie hoe je meerdere tasks kan submitten naar de ExecutorService.
 		// Hierbij wordt gewacht tot het resultaat terugkomt van de task die als eerste compleet is.
-		System.out.println(executorServiceSingle.invokeAny(Arrays.asList(task1, task2)));
+		System.out.println(
+			"Result: " + executorServiceSingle.invokeAny(Arrays.asList(task1, task2))
+		);
 
+		// Als je wilt dat alle tasks uitgevoerd moeten worden, dan kan je invokeAll gebruiken
+		List<Future<String>> resultFutures = executorServiceSingle.invokeAll(Arrays.asList(task1, task2));
 
+		// De futures die je terugkrijgt van invokeAll zijn in dezelfde volgorde als de tasks die je meegaf als param
+		for (Future<String> x : resultFutures) {
+			System.out.println(x.get());
+		}
 
 		// Zoals altijd tenminste een shutdown.
 		executorServiceSingle.shutdown();
